ec2fc44619cd4e7eb9b33e993b587a3ea408d1c9
Caroly91 <psicopedagogiaconcarol@gmail.com>
2026-03-28 11:22:25 -0300

    Inicialización del Portafolio

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("btnProyecto").addEventListener("click", function() {
+  window.location.href = "https://caroly91.github.io/AlkeWalletNew/";
+});
+
+document.getElementById("btnRepo").addEventListener("click", function() {
+  window.location.href = "https://github.com/Caroly91/AlkeWalletNew";
+});
